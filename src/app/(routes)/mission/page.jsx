b72b0e841a91a3691dea75d7b103c3877366c590
treyktw <101672317+treyktw@users.noreply.github.com>
2024-05-01 20:08:20 -0400

    animate fix

--- src/app/(routes)/mission/page.jsx
+++ src/app/(routes)/mission/page.jsx
@@ -3,52 +3,41 @@ import styles from "./page.module.css";
 import { MaskText } from "./_components/mask-text";
 import { useEffect, useRef } from "react";
 
-
-
 export default function Home() {
   const container = useRef(null);
   const stickyMask = useRef(null);
 
-  const initialMaskSize = .8;
+  const initialMaskSize = 0.8;
 
-const targetMaskSize = 30;
+  const targetMaskSize = 30;
 
+  
 
-
-useEffect( () => {
-
-  requestAnimationFrame(animate)
-
-}, [animate])
-
-
-
-// eslint-disable-next-line react-hooks/exhaustive-deps
-const animate = useCallback(() => {
-
-  if (stickyMask.current) {
-    const maskSizeProgress = targetMaskSize * getScrollProgress();
-    stickyMask.current.style.webkitMaskSize = `${(initialMaskSize + maskSizeProgress) * 100}%`;
+  const animate = useCallback(() => {
+    const getScrollProgress = () => {
+      if (stickyMask.current && container.current) {
+        const containerHeight = container.current.getBoundingClientRect().height;
+        const scrollProgress =
+          stickyMask.current.offsetTop / (containerHeight - window.innerHeight);
+        return scrollProgress;
+      }
+      return 0;
+    };
+  
+    if (stickyMask.current) {
+      const maskSizeProgress = targetMaskSize * getScrollProgress();
+      stickyMask.current.style.webkitMaskSize = `${
+        (initialMaskSize + maskSizeProgress) * 100
+      }%`;
+      requestAnimationFrame(animate);
+    }
+  }, [targetMaskSize, initialMaskSize]);
+  
+  useEffect(() => {
     requestAnimationFrame(animate);
-  }
-});
+  }, [animate]);
 
-
-
-
-
-const getScrollProgress = () => {
-
-  if (stickyMask.current && container.current) {
-    const containerHeight = container.current.getBoundingClientRect().height;
-    const scrollProgress = stickyMask.current.offsetTop / (containerHeight - window.innerHeight);
-    return scrollProgress;
-  }
-  return 0;
-
-}
-
-return (
+  return (
     <main className={styles.main}>
       <div ref={container} className={styles.container}>
         <div ref={stickyMask} className={styles.stickyMask}>
